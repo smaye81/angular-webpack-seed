ca89746d9229816a778b9e72511b20886c4f36ca
Steve Ayers <steve.ayers@firstinsight.com>
2015-08-21 17:01:26 -0400

    Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
 var WebpackNotifierPlugin = require('webpack-notifier');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
 var appConfig = {
     WEBPACK_ENTRY : ['./app/app.js'],
-    BUNDLE_NAME : './app/bundle.js'
+    BUNDLE_NAME : {
+        APP : './app/bundle.js',
+        VENDOR : './app/vendor.bundle.js'
+    }
 };
 
 module.exports = {
-    entry: appConfig.WEBPACK_ENTRY,
+    entry: {
+        'app': appConfig.WEBPACK_ENTRY,
+        'vendor': ["angular"]
+    },
     output: {
         path: __dirname,
-        filename: appConfig.BUNDLE_NAME
+        filename: appConfig.BUNDLE_NAME.APP
     },
     plugins : [
-        new WebpackNotifierPlugin()
+        new WebpackNotifierPlugin(),
+        new CommonsChunkPlugin("vendor", appConfig.BUNDLE_NAME.VENDOR)
     ],
     module: {
         loaders: [
